2b09ad1c586e065a35b566d20209625a25230208
Kaio Vinicius <kaio.viniciuspv@gmail.com>
2022-02-21 10:15:29 -0300

    Feat(TurmasController): add Data Filter

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,31 +1,51 @@
-const { NUMBER } = require('sequelize')
+const {
+    NUMBER
+} = require('sequelize')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
 const database = require('../models')
 
 class TurmaController {
     static async pegaTodasAsTurmas(req, res) {
-        paranoid: false
+        const {
+            data_inicial,
+            data_final
+        } = req.query
+        const where = {}
+
+        data_inicial || data_final ? where.data_inicio = {} : null
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null
+        data_final ? where.data_inicio[Op.lte] = data_final : null
+
         try {
-            const todasAsTurmas = await database.Turmas.findAll()
+            const todasAsTurmas = await database.Turmas.findAll({
+                where
+            })
             return res.status(200).json(todasAsTurmas)
         } catch (error) {
             return res.status(500).json(error.message)
         }
+        paranoid: false
     }
 
     static async pegaUmaTurma(req, res) {
-        const { id } = req.params
+        const {
+            id
+        } = req.params
         try {
             const umaTurma = await database.Turmas.findOne({
                 where: {
                     id: Number(id)
-                }})
+                }
+            })
             return res.status(200).json(umaTurma)
         } catch (error) {
             return res.status(500).json(error.message)
         }
     }
 
-    static async criaTurma(req,res) {
+    static async criaTurma(req, res) {
         const novaTurma = req.body
         try {
             const novaTurmaCriada = await database.Turmas.create(novaTurma)
@@ -35,26 +55,41 @@ class TurmaController {
         }
     }
 
-    static async atualizarTurma(req,res) {
-        const { id } = req.params
+    static async atualizarTurma(req, res) {
+        const {
+            id
+        } = req.params
         const novaInfo = req.body
         try {
-            await database.Turmas.update(novaInfo, {where: { id: Number(id)}})
+            await database.Turmas.update(novaInfo, {
+                where: {
+                    id: Number(id)
+                }
+            })
             const turmaAtualizada = await database.Turmas.findOne({
                 where: {
                     id: Number(id)
-                }})
+                }
+            })
             return res.status(200).json(turmaAtualizada)
         } catch (error) {
             return res.status(500).json(error.message)
         }
     }
 
-    static async deletarTurma(req,res) {
-        const { id } = req.params
+    static async deletarTurma(req, res) {
+        const {
+            id
+        } = req.params
         try {
-            await database.Turmas.destroy({where: { id: Number(id)}})
-            return res.status(200).json({mensagem : `id ${id} deletado`})
+            await database.Turmas.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json({
+                mensagem: `id ${id} deletado`
+            })
         } catch (error) {
             return res.status(500).json(error.message)
         }
